b80f13fa036ae5f9ac36cc09f4b9c34122f005c8
AnastasiaDMW <AnastasiaZVV1@gmail.com>
2024-05-17 15:03:16 +0300

    Added user favorite and assembly PC

--- client/src/components/Profile/Configurations_PC.jsx
+++ client/src/components/Profile/Configurations_PC.jsx
@@ -1,21 +1,80 @@
-import pc_image from '../../../src/images/svg/white_square.svg'
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
-export default function ConfigurationPC(){
+
+export default function ConfigurationPC() {
+
+  const baseUrl = "http://localhost:9090/api/component/fileSystem/"
+  const [userEmail, setUserEmail] = useState(() => {
+    const storedEmail = localStorage.getItem('userEmail');
+    return storedEmail ? storedEmail : "";
+  });
+  const [userAssemblies, setUserAssemblies] = useState([]);
+  const [userData, setUserData] = useState({
+    id: 0,
+    name: "",
+    lastname: "",
+    email: "",
+    photo: "CatDefaultAvatar.png",
+    bonuses: 0,
+    phoneNumber: "",
+    assemblies: []
+  })
+
+  useEffect(() => {
+    console.log(userEmail);
+    if (userData.id === 0) {
+      getUser()
+    }
+  }, [userEmail]);
+
+  useEffect(() => {
+    console.log(userData);
+    fetchData()
+  }, [userData]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:9090/api/user/user_assembly', {
+        params: {
+          userAssemblyId: userData.id
+        }
+      });
+      const data = response.data;
+      setUserAssemblies(data.assemblies);
+    } catch (error) {
+      console.error('Ошибка при загрузке данных:', error);
+    }
+  };
+
+  const getUser = async (e) => {
+    try {
+      const response = await axios.get("http://localhost:9090/api/user/user_email", {
+        params: {
+          email: userEmail,
+        }
+      });
+      const user = response.data;
+      setUserData(user);
+      localStorage.setItem('userData', userData);
+    } catch (err) {
+      console.error("Ошибка при получении данных", err);
+    }
+  }
+
   return <div className='pc-config'>
     <h3 className='pc-config__title blue-title'>Мои сборки</h3>
-    <ul className='pc-config__configs-list configs-list'>
-      <li className="configs-list__item">
-        <span className="configs-list__id">AOIS 89364043</span>
-        <img src={pc_image} alt="" className="configs-list__img"/>
+      <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }} className='pc-config__configs-list configs-list' >
+      { userAssemblies && userAssemblies.map((assembly) => (
+        <li className="configs-list__item" key={assembly?.id}>
+          <span className="configs-list__id">{assembly?.title}</span>
+          <img
+            src={`${baseUrl}${assembly?.images}`}
+            alt={`Изображение сборки ${assembly?.id}`}
+            className="configs-list__img"
+          />
       </li>
-      <li className="configs-list__item">
-        <span className="configs-list__id">AOIS 89364043</span>
-        <img src={pc_image} alt="" className="configs-list__img"/>
-      </li>
-      <li className="configs-list__item">
-        <span className="configs-list__id">AOIS 89364043</span>
-        <img src={pc_image} alt="" className="configs-list__img"/>
-      </li>
-    </ul>
+      ))}
+      </ul>
   </div>
 }
--- client/src/components/Profile/Favorite__Products.jsx
+++ client/src/components/Profile/Favorite__Products.jsx
@@ -1,45 +1,86 @@
 import pc_image from '../../../src/images/svg/white_square.svg'
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function FavoriteProducts(){
+
+  const baseUrl = "http://localhost:9090/api/component/fileSystem/"
+  const [userEmail, setUserEmail] = useState(() => {
+    const storedEmail = localStorage.getItem('userEmail');
+    return storedEmail ? storedEmail : "";
+  });
+  const [userFavorites, setUserFavorites] = useState([]);
+  const [userData, setUserData] = useState({
+    id: 0,
+    name: "",
+    lastname: "",
+    email: "",
+    photo: "CatDefaultAvatar.png",
+    bonuses: 0,
+    phoneNumber: "",
+    assemblies: []
+  })
+
+  useEffect(() => {
+    console.log(userEmail);
+    if (userData.id === 0) {
+      getUser()
+    }
+  }, [userEmail]);
+
+  useEffect(() => {
+    console.log(userData);
+    fetchData()
+  }, [userData]);
+
+  useEffect(() => {
+    console.log(userFavorites);
+  }, [userFavorites]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:9090/api/user/user_favorite', {
+        params: {
+          userId: userData.id
+        }
+      });
+      const data = response.data;
+      setUserFavorites(data.carts);
+    } catch (error) {
+      console.error('Ошибка при загрузке данных:', error);
+    }
+  };
+
+  const getUser = async (e) => {
+    try {
+      const response = await axios.get("http://localhost:9090/api/user/user_email", {
+        params: {
+          email: userEmail,
+        }
+      });
+      const user = response.data;
+      setUserData(user);
+      localStorage.setItem('userData', userData);
+    } catch (err) {
+      console.error("Ошибка при получении данных", err);
+    }
+  }
+
   return <div className="fav-products">
     <h3 className="fav-products__title blue-title">Избранные товары</h3>
-    <ul className='fav-products__products-list products-list'>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">1</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">2</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">3</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">4</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">5</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">6</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">7</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">8</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-      <li className="products-list__item">
-        {/*<span className="products-list__id">9</span>*/}
-        <img src={pc_image} alt="" className="products-list__img"/>
-      </li>
-    </ul>
+    <div className='fav-products__products-list products-list'>
+      { userFavorites && userFavorites.map((item, index) => {
+        return (
+          <div className="products-list__item" key={index}>
+            <span className="configs-list__id">{item.assemblies.title}</span>
+            <img
+              src={`${baseUrl}${item.assemblies.images}`}
+              alt={`Изображение сборки ${item.assemblies.id}`}
+              className="configs-list__img"
+            />
+          </div>
+        );
+      })}
+    </div>
   </div>
 }
